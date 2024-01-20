5bddea809628147b82e437045e071b1eb0d0fb80
Norbert Kéri <keri.norbert.miklos@gmail.com>
2024-01-20 12:58:16 +0100

    feat(diffview): add some bindings

--- nvim/lua/visko/plugins/diffview.lua
+++ nvim/lua/visko/plugins/diffview.lua
@@ -2,8 +2,17 @@ return {
     { 'sindrets/diffview.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
+            local actions = require("diffview.actions")
             require("diffview").setup({
                 enhanced_diff_hl = true,
+                keymaps = {
+                    view = {
+                        -- keep tab as tabnext instead of jumping to next file
+                        ["<tab>"] = false,
+                        ["<leader>d"] = actions.cycle_layout,
+                        ["h"] = function() vim.cmd("DiffviewFileHistory %") end,
+                    },
+                },
                 hooks = {
                     diff_buf_read = function(bufnr) 
                         vim.cmd("norm! zi")

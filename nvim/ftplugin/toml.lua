862b6c11d656d7d63502217cb89b5a0d8386708b
Norbert Kéri <keri.norbert.miklos@gmail.com>
2024-05-21 00:37:01 +0200

    feat: add Cargo.toml related mappings

---
+++ nvim/ftplugin/toml.lua
@@ -0,0 +1,10 @@
+if vim.fn.expand('%:t') == 'Cargo.toml' then
+    local crates = require("crates")
+    local opts = { silent = true }
+
+    vim.keymap.set("n", "<leader>g", crates.show_dependencies_popup, opts)
+    vim.keymap.set("n", "<leader>h", crates.open_homepage, opts)
+    vim.keymap.set("n", "<leader>d", crates.open_documentation, opts)
+    vim.keymap.set("n", "<leader>r", crates.open_repository, opts)
+    vim.keymap.set("n", "<leader>c", crates.open_crates_io, opts)
+end

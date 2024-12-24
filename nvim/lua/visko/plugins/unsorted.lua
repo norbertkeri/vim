a4a6dd04128026c8956642202718c81030f52e0b
Norbert Kéri <keri.norbert.miklos@gmail.com>
2024-12-25 00:10:19 +0100

    feat: remove neoai

--- nvim/lua/visko/plugins/unsorted.lua
+++ nvim/lua/visko/plugins/unsorted.lua
@@ -186,34 +186,6 @@ local plugins = {
         end,
     },
     {
-        "Bryley/neoai.nvim",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-        },
-        cmd = {
-            "NeoAI",
-            "NeoAIOpen",
-            "NeoAIClose",
-            "NeoAIToggle",
-            "NeoAIContext",
-            "NeoAIContextOpen",
-            "NeoAIContextClose",
-            "NeoAIInject",
-            "NeoAIInjectCode",
-            "NeoAIInjectContext",
-            "NeoAIInjectContextCode",
-        },
-        keys = {
-            { "<leader>as", desc = "summarize text" },
-            { "<leader>ag", desc = "generate git message" },
-        },
-        config = function()
-            require("neoai").setup({
-                -- Options go here
-            })
-        end,
-    },
-    {
         "gbprod/yanky.nvim",
         config = function()
             local map = require("visko.helpers").vim.mapkey
